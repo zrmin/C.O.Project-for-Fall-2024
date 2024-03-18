eda42e0da20a7a14ea4f113db549168c6c386343
zrmin <fmrt19zrmin@163.com>
2024-03-18 21:37:51 +0800

    Standardize the code format

--- Lab1/useful_functions.c
+++ Lab1/useful_functions.c
@@ -29,20 +29,19 @@
  */
 #include <stdlib.h>
 
-int
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-    char *prgName   = NULL;
-    char *iFileName = NULL;
-    char *oFileName = NULL;
+        char *prgName   = NULL;
+        char *iFileName = NULL;
+        char *oFileName = NULL;
 
-    prgName   = argv[0];
-    iFileName = argv[1];
-    oFileName = argv[2];
+        prgName   = argv[0];
+        iFileName = argv[1];
+        oFileName = argv[2];
 
-    printf("program name = '%s'\n", prgName);
-    printf("input file name = '%s'\n", iFileName);
-    printf("output file name = '%s'\n", oFileName);
+        printf("program name = '%s'\n", prgName);
+        printf("input file name = '%s'\n", iFileName);
+        printf("output file name = '%s'\n", oFileName);
 }
 
 /*
@@ -70,26 +69,26 @@ main(int argc, char* argv[])
 FILE *infile = NULL;
 FILE *outfile = NULL;
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    /* open the source file */
-    infile = fopen(argv[1], "r");
-    outfile = fopen(argv[2], "w");
+        /* open the source file */
+        infile = fopen(argv[1], "r");
+        outfile = fopen(argv[2], "w");
 
-    if (!infile) {
-        printf("Error: Cannot open file %s\n", argv[1]);
-        exit(4);
-    }
-    if (!outfile) {
-        printf("Error: Cannot open file %s\n", argv[2]);
-        exit(4);
-    }
+        if (!infile) {
+                printf("Error: Cannot open file %s\n", argv[1]);
+                exit(4);
+        }
 
-    /* Do stuff with files */
+        if (!outfile) {
+                printf("Error: Cannot open file %s\n", argv[2]);
+                exit(4);
+        }
 
-    fclose(infile);
-    fclose(outfile);
+        /* Do stuff with files */
+
+        fclose(infile);
+        fclose(outfile);
 }
 
 
@@ -101,131 +100,139 @@ main(int argc, char *argv[])
  * and decimal numbers must be in the form "#30".
  *
  */
-int
-toNum(char *pStr)
+int toNum(char *pStr)
 {
-    char *t_ptr;
-    char *orig_pStr;
-    int t_length, k;
-    int lNum, lNeg = 0;
-    long int lNumLong;
+        char *t_ptr;
+        char *orig_pStr;
+        int t_length, k;
+        int lNum, lNeg = 0;
+        long int lNumLong;
 
-    orig_pStr = pStr;
-    if(*pStr == '#') {           /* decimal */ 
-        pStr++;
-        if(*pStr == '-') {        /* dec is negative */
-            lNeg = 1;
-            pStr++;
+        orig_pStr = pStr;
+        if (*pStr == '#') {           /* decimal */ 
+                pStr++;
+                if (*pStr == '-') {        /* dec is negative */
+                        lNeg = 1;
+                        pStr++;
+                }
+
+                t_ptr = pStr;
+                t_length = strlen(t_ptr);
+
+                for (k=0; k < t_length; k++) {
+                        if (!isdigit(*t_ptr)) {
+                                printf("Error: invalid decimal operand, %s\n",orig_pStr);
+                                exit(4);
+                        }
+                        t_ptr++;
+                }
+
+                lNum = atoi(pStr);
+                if (lNeg)
+                        lNum = -lNum;
+
+                return lNum;
+        } else if (*pStr == 'x') {           /* hex */
+                pStr++;
+                if (*pStr == '-') {              /* hex is negative */
+                        lNeg = 1;
+                        pStr++;
+                }
+
+                t_ptr = pStr;
+                t_length = strlen(t_ptr);
+                for (k=0; k < t_length; k++) {
+                        if (!isxdigit(*t_ptr)) {
+                                printf("Error: invalid hex operand, %s\n", orig_pStr);
+                                exit(4);
+                        }
+                        t_ptr++;
+                }
+                /* convert hex string into integer */
+                lNumLong = strtol(pStr, NULL, 16); 
+                lNum = (lNumLong > INT_MAX) ? INT_MAX : lNumLong;
+                if (lNeg)
+                        lNum = -lNum;
+                return lNum;
+        } else {
+                printf("Error: invalid operand, %s\n", orig_pStr);
+                exit(4);  
         }
-
-        t_ptr = pStr;
-        t_length = strlen(t_ptr);
-
-        for(k=0; k < t_length; k++) {
-            if (!isdigit(*t_ptr)) {
-                printf("Error: invalid decimal operand, %s\n",orig_pStr);
-                exit(4);
-            }
-            t_ptr++;
-        }
-
-        lNum = atoi(pStr);
-        if (lNeg)
-            lNum = -lNum;
-
-        return lNum;
-    } else if(*pStr == 'x') {           /* hex */
-        pStr++;
-        if(*pStr == '-') {              /* hex is negative */
-            lNeg = 1;
-            pStr++;
-        }
-
-        t_ptr = pStr;
-        t_length = strlen(t_ptr);
-        for(k=0; k < t_length; k++) {
-            if (!isxdigit(*t_ptr)) {
-                printf("Error: invalid hex operand, %s\n", orig_pStr);
-                exit(4);
-            }
-            t_ptr++;
-        }
-        /* convert hex string into integer */
-        lNumLong = strtol(pStr, NULL, 16); 
-        lNum = (lNumLong > INT_MAX) ? INT_MAX : lNumLong;
-        if(lNeg)
-            lNum = -lNum;
-        return lNum;
-    } else {
-        printf("Error: invalid operand, %s\n", orig_pStr);
-        exit(4);  
-    }
 }
 
 
 
-/* Fourth.
+/* 
+ * Fourth.
  * Parsing Assembly Language
  * Take a line of the input file and parse it into corresponding fields.
  * Note that you need to write the isOpcode(char*) function 
  * which determines whether a string of characters is a valid opcode.
+ *
  */
 #define MAX_LINE_LENGTH 255
 enum
 {
-    DONE, OK, EMPTY_LINE
+        DONE,
+        OK,
+        EMPTY_LINE
 };
 
-int
-readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
-        ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
-        )
+int readAndParse(FILE *pInfile, char *pLine, char **pLabel, \
+                char **pOpcode, char **pArg1, char **pArg2, \
+                char **pArg3, char **pArg4)
 {
-    char * lRet, * lPtr;
-    int i;
-    if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
-        return( DONE );
-    for( i = 0; i < strlen( pLine ); i++ )
-        pLine[i] = tolower( pLine[i] );
+        char *lRet, *lPtr;
+        int i;
 
-    /* convert entire line to lowercase */
-    *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+        if (!fgets( pLine, MAX_LINE_LENGTH, pInfile))
+                return (DONE);
 
-    /* ignore the comments */
-    lPtr = pLine;
+        for (i = 0; i < strlen(pLine); i++)
+                pLine[i] = tolower(pLine[i]);
 
-    while( *lPtr != ';' && *lPtr != '\0' &&
-            *lPtr != '\n' ) 
-        lPtr++;
+        /* convert entire line to lowercase */
+        *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 \
+                = *pArg4 = pLine + strlen(pLine);
 
-    *lPtr = '\0';
-    if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
-        return( EMPTY_LINE );
+        /* ignore the comments */
+        lPtr = pLine;
 
-    if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
-    {
-        *pLabel = lPtr;
-        if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-    }
+        while (*lPtr != ';' && *lPtr != '\0' && *lPtr != '\n') 
+                lPtr++;
 
-    *pOpcode = lPtr;
+        *lPtr = '\0';
+        if (!(lPtr = strtok( pLine, "\t\n ,"))) 
+                return (EMPTY_LINE);
 
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+        if (isOpcode(lPtr) == -1 && lPtr[0] != '.') { /* found a label */
+                *pLabel = lPtr;
+                if (!(lPtr = strtok(NULL, "\t\n ,")))
+                        return (OK);
+        }
 
-    *pArg1 = lPtr;
+        *pOpcode = lPtr;
 
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+        if (!(lPtr = strtok( NULL, "\t\n ,")))
+                return (OK);
 
-    *pArg2 = lPtr;
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+        *pArg1 = lPtr;
 
-    *pArg3 = lPtr;
+        if (!(lPtr = strtok(NULL, "\t\n ,")))
+                return (OK);
 
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+        *pArg2 = lPtr;
+        if (!(lPtr = strtok(NULL, "\t\n ,")))
+                return( OK );
 
-    *pArg4 = lPtr;
+        *pArg3 = lPtr;
 
-    return( OK );
+        if (!(lPtr = strtok(NULL, "\t\n ,")))
+                return (OK);
+
+        *pArg4 = lPtr;
+
+        return (OK);
 }
 
 
@@ -233,35 +240,36 @@ readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
 /* To call readAndParse, you would use the following: */
 void func() 
 {
-    char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1,
-         *lArg2, *lArg3, *lArg4;
+        char lLine[MAX_LINE_LENGTH + 1];
+        char *lLabel, *lOpcode, *lArg1, *lArg2, *lArg3, *lArg4;
+        int lRet;
+        FILE *lInfile;
 
-    int lRet;
+        lInfile = fopen("data.in", "r"); /* open the input file */
 
-    FILE * lInfile;
-
-    lInfile = fopen( "data.in", "r" );	/* open the input file */
-
-    do
-    {
-        lRet = readAndParse( lInfile, lLine, &lLabel,
-                &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
-        if(lRet != DONE && lRet != EMPTY_LINE)
+        do
         {
-            // ...
-        }
-    } while(lRet != DONE);
+                lRet = readAndParse(lInfile, lLine, &lLabel, &lOpcode, \
+                                    &lArg1, &lArg2, &lArg3, &lArg4);
+                if(lRet != DONE && lRet != EMPTY_LINE)
+                {
+                        // ...
+                }
+        } while(lRet != DONE);
 }
 
 
 
-/* Fifth.
+/* 
+ * Fifth.
  * File Output
  * To write to your output file, you can use the following:
+ * 
  */
 FILE *pOutfile;
 pOutfile = fopen("data.out", "w");
 
 // ...
 
-fprintf(pOutfile, "0x%.4X\n", lInstr);	/* where lInstr is declared as an int */
+/* where lInstr is declared as an int */
+fprintf(pOutfile, "0x%.4X\n", lInstr);
