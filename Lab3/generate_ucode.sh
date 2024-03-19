a465615592ca0fb048c17981867bf298e6e2415c
zrmin <fmrt19zrmin@163.com>
2024-03-19 17:09:40 +0800

    Coding style: fix lc3bsim3.c coding style

--- Lab3/generate_ucode.sh
+++ Lab3/generate_ucode.sh
@@ -5,7 +5,7 @@ set -e
 # 输入文件路径
 input_file="ucode_original"
 
-# 提取每行的0和1，保存到ucode3文件中
+# 提取每行的0和1，保存到ucode文件中
 awk '{gsub(/[^01]/, ""); print}' "$input_file" | awk '{ print }' | sed '/^$/d' > ucode
 
 echo "0 and 1 have been extracted and saved to ucode"
